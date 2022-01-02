c9e2b36240ea7519781cbcc88c41753f4ec9c7f9
TheophanesCampos <theophanes.asos@gmail.com>
2022-01-02 10:50:44 -0300

    adicionado método de busca por nome da descricao

--- o-obra/src/main/java/br/com/o_obra/orcamento/controllers/GreetingsController.java
+++ o-obra/src/main/java/br/com/o_obra/orcamento/controllers/GreetingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,34 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.o_obra.orcamento.model.ComposicaoSintetica;
 import br.com.o_obra.orcamento.repository.ComposicaoSinteticaRepository;
 
-/**
- *
- * A sample greetings controller to return greeting text
- */
 @RestController
 public class GreetingsController {
 	@Autowired /* IC/CD ou CDI - Injeção de dependencia*/
 	private ComposicaoSinteticaRepository composicaoSinteticaRepository;
-	
-	
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Olá " + name + "!";
-    }
     
     
-    
-  //Primeiro método de API - buscarTodos   
+  //lista todas as composicoes sinteticas do banco de dados
     @GetMapping(value="listatodos")
     @ResponseBody /*retorna os dados para o corpo da resposta*/
     public ResponseEntity<List<ComposicaoSintetica>> listaComposicoes(){
     	List<ComposicaoSintetica> composicoes = composicaoSinteticaRepository.findAll(); /*executa a consulta no banco de dados*/
     	return new ResponseEntity<List<ComposicaoSintetica>>(composicoes,HttpStatus.OK);/*retorna a lista em json*/
+    }
+    
+    @GetMapping(value = "buscarpordescricaocomposicao") /*Mapeia a url*/
+    @ResponseBody /*Descrição da resposta*/
+    public ResponseEntity<List<ComposicaoSintetica>> buscarPorDescricaoComposicao(@RequestParam(name = "descricaoComposicao") String name){/*Recebe os dados do usuário a ser consultado*/
+    	
+    	List<ComposicaoSintetica> composicoes = composicaoSinteticaRepository.buscarPorDescricaoComposicao(name.trim().toUpperCase());
+    	
+    	return new ResponseEntity<List<ComposicaoSintetica>>(composicoes, HttpStatus.OK);	
     }
 }
--- o-obra/src/main/java/br/com/o_obra/orcamento/repository/ComposicaoSinteticaRepository.java
+++ o-obra/src/main/java/br/com/o_obra/orcamento/repository/ComposicaoSinteticaRepository.java
@@ -1,10 +1,17 @@
 package br.com.o_obra.orcamento.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.o_obra.orcamento.model.ComposicaoSintetica;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ComposicaoSinteticaRepository extends JpaRepository<ComposicaoSintetica, Long> {
+	
+	@Query(value = "select c from ComposicaoSintetica c where upper(trim(c.descricaoComposicao)) like %?1%")
+	List<ComposicaoSintetica> buscarPorDescricaoComposicao(String name);
 
 }
